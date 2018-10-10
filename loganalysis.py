ae053b51565a7141609e8b82e595fa7b2f2acacc
Chirag Srivastava <chirag.srivastava@live.com>
2018-10-10 03:30:26 +0000

    Changes after review

--- loganalysis.py
+++ loganalysis.py
@@ -1,84 +1,136 @@
 #!/usr/bin/env python3
-
 import psycopg2
 
-# This is a function that takes PostgreSQL queries
+# The name of database being connected
+DBNAME = "news"
 
 
-def analysis(query):
-    conn = psycopg2.connect(dbname="news")
+def answer1():
+    # A function for answering question 1
+
+    print("\nThe most popular articles are\n")
+
+    conn = psycopg2.connect(dbname=DBNAME)
     cursor = conn.cursor()
 
     # This view counts the ip hence giving no. of views
-    ipcounter = "create view ipcounter as select path, count(ip) " \
-                "as vw from log group by path order by vw desc;"
+    ipcounter = """create view ipcounter as select path, count(ip)
+                as vw from log group by path order by vw desc;"""
     cursor.execute(ipcounter)
 
     # This view joins ipview and author table
-    artcounter = "create view artcounter as select articles.title," \
-                 " ipcounter.vw from " \
-                 "articles left join ipcounter on " \
-                 "ipcounter.path LIKE '%' || " \
-                 "articles.slug || '%' order by vw desc;"
+    artcounter = """create view artcounter as select articles.title,
+                  ipcounter.vw from
+                  articles left join ipcounter on
+                 ipcounter.path = CONCAT('/article/', articles.slug)
+                  order by vw desc;"""
     cursor.execute(artcounter)
 
-    # This view joins articles and authors
-    artauth = "create view artauth as select articles.title, " \
-              "authors.name from articles join authors on " \
-              "authors.id = articles.author;"
-    cursor.execute(artauth)
+    pop_query = "select * from artcounter limit 5;"
+    cursor.execute(pop_query)
 
-    # This view counts no. of requests made at a particular day
-    request = " create view request as select time::timestamp::date," \
-              " count(*) as requests " \
-              "from log group by time::timestamp::date;"
-    cursor.execute(request)
-
-    # This view displays errors occurred in a day
-    errorpd = " create view errorpd as select time::timestamp::date," \
-              " count(time::timestamp::date) as errors " \
-              "from log where status='404 NOT FOUND' " \
-              "group by time::timestamp::date;"
-    cursor.execute(errorpd)
-
-    # This view displays requests and errors occurred
-    #  in a day by joining errorpd and request
-    requesterror = "create view requesterror as select request.time," \
-                   " request.requests, errorpd.errors " \
-                   "from request left join errorpd on" \
-                   " request.time=errorpd.time;"
-    cursor.execute(requesterror)
-
-    cursor.execute(query)
-
-    # That's how our result gets printed
+    # That's how result of queries gets printed
     result = cursor.fetchall()
-    print(result)
+    for title, views in result:
+        print("{} -- {} views".format(title, views))
 
     conn.close()
 
 
-# This query goes into function and returns
-#  top five most popular articles and their views
-print("\nHere are top five most popular articles:\n")
-pop_query = "select * from artcounter limit 5;"
-analysis(pop_query)
+def answer2():
 
-# This query goes into function and returns
-# top five most popular articles and their views
-print("\nHere are top five most popular authors:\n")
-pop_auth_query = "select artauth.name, artcounter.vw from" \
-                 " artcounter join artauth on " \
-                 "artauth.title = artcounter.title" \
-                 " order by artcounter.vw desc limit 5;"
-analysis(pop_auth_query)
+    # A function for answering question 2
 
-# This query goes into function and returns days
-#  when more than 1% of requests lead to error
-print("\nHere are the days when more "
-      "than 1% of requests lead to error\n")
-percentage_error_query = "select * from (select time::timestamp::date," \
-                         " (errors + 0.0)*100/requests " \
-                         "as percentage_error from requesterror) as final" \
-                         " where percentage_error > 1;"
-analysis(percentage_error_query)
+    print("\nThe most popular authors are\n")
+
+    conn = psycopg2.connect(dbname=DBNAME)
+    cursor = conn.cursor()
+
+    # This view counts the ip hence giving no. of views
+    ipcounter = """create view ipcounter as select path, count(ip)
+                    as vw from log group by path order by vw desc;"""
+    cursor.execute(ipcounter)
+
+    # This view joins ipview and author table
+    artcounter = """create view artcounter as select articles.title,
+                    ipcounter.vw from
+                     articles left join ipcounter on
+                     ipcounter.path = CONCAT('/article/', articles.slug)
+                      order by vw desc;"""
+    cursor.execute(artcounter)
+
+    # This view joins articles and authors
+    artauth = """create view artauth as select articles.title,
+              authors.name from articles join authors on
+              authors.id = articles.author;"""
+    cursor.execute(artauth)
+
+    pop_auth_query = """select artauth.name, sum(artcounter.vw)
+                    as total_views from
+                    artcounter join artauth on
+                    artauth.title = artcounter.title
+                    group by artauth.name order by total_views desc;"""
+    cursor.execute(pop_auth_query)
+
+    # That's how result of queries gets printed
+    result = cursor.fetchall()
+    for title, views in result:
+        print("{} -- {} views".format(title, views))
+
+    conn.close()
+
+
+def answer3():
+
+    # A function for answering question 3
+
+    print("""\nThe days when more than 1% of requests lead to errors are\n""")
+
+    conn = psycopg2.connect(dbname=DBNAME)
+    cursor = conn.cursor()
+
+    # This view counts no. of requests made at a particular day
+    request = """create view request as select time::timestamp::date,
+               count(*) as requests
+              from log group by time::timestamp::date;"""
+    cursor.execute(request)
+
+    # This view displays errors occurred in a day
+    errorpd = """create view errorpd as select time::timestamp::date,
+               count(time::timestamp::date) as errors
+              from log where status!='200 OK'
+              group by time::timestamp::date;"""
+    cursor.execute(errorpd)
+
+    # This view displays requests and errors occurred
+    #  in a day by joining errorpd and request
+    requesterror = """create view requesterror as select request.time,
+                    request.requests, errorpd.errors
+                   from request left join errorpd on
+                    request.time=errorpd.time;"""
+    cursor.execute(requesterror)
+
+    percentage_error_query = """select * from (select time::timestamp::date,
+                              (errors + 0.0)*100/requests
+                              as percentage_error from requesterror) as final
+                              where percentage_error > 1;"""
+
+    cursor.execute(percentage_error_query)
+
+    # That's how result of queries gets printed
+    result = cursor.fetchall()
+    for title, views in result:
+        print("{} -- {} errors".format(title, views))
+
+    conn.close()
+
+
+def run():
+
+    # This function returns all the answers
+
+    answer1()
+    answer2()
+    answer3()
+
+run()
--- readme.md
+++ readme.md
@@ -1,29 +1,48 @@
-#h1 Logs Analysis Project
+# Logs Analysis Project
 
-This is a simple project that builds informative summary from logs
-of a live database.
-
-#h2 Installation
-
-1. Set up a virtual machine using Vagrant and VirtualBox, for help
-    visit https://medium.com/@JohnFoderaro/how-to-set-up-a-local-linux-environment-with-vagrant-163f0ba4da77
-
-2. Turn on and log in to your virtual machine
-    `vagrant up`
-    `vagrant ssh`
-
-3. Navigate to your vagrant directory
-    `cd /vagrant`
-
-4. Clone or download the Github Repository to your vagrant directory
-   and add 'newsdata.sql' database (Not in repository, due to large size) to 
-    the directory.
+This project sets up a PostgreSQL database for a fictional news website.
 
 
-5. Run the file 'loganalysis.py'
+## Getting Started
+
+### The database
+articles - This table contains a database of all the articles published
+authors -  This table contains a database of all the authors including their name
+log - This table has a database row for each time a reader access a web page.
+
+### The Questions
+1. What are the most popular three articles of all time?
+2. Who are the most popular article authors of all time?
+3. On which days did more than 1% of requests lead to errors?
+
+
+## Installation
+
+1. Set up a virtual machine using Vagrant and VirtualBox
+  Install VirtualBox https://www.virtualbox.org/wiki/Download_Old_Builds_5_1
+  Install Vagrant https://www.vagrantup.com/downloads.html
+
+  for help, visit https://medium.com/@JohnFoderaro/how-to-set-up-a-local-linux-environment-with-vagrant-163f0ba4da77
+
+2. From your terminal, inside the vagrant subdirectory, run the command
+  `vagrant up`. This will cause Vagrant to download the Linux operating system
+  and install it. This may take quite a while (many minutes) depending on how
+  fast your Internet connection is.
+
+3. When vagrant up is finished running, you will get your shell prompt back.
+  At this point, you can run `vagrant ssh` to log in to your newly
+  installed Linux VM
+
+4. Inside the VM change directory to `cd /vagrnat`
+
+5. Clone or download the Github Repository to your vagrant directory
+
+6.  Add 'newsdata.sql' database to the directory from       https://d17h27t6h515a5.cloudfront.net/topher/2016/August/57b5f748_newsdata/newsdata.zip
+
+7. Run the file 'loganalysis.py'
     `python loganalysis.py`
 
-6. To explore the database 'newsdata.sql'
+8. To explore the database 'newsdata.sql'
     `psql -d news -f newsdata.sql`
 
 #h2 Additional Information
@@ -33,26 +52,26 @@ The views that are used in this project are:
 `create view ipcounter as select path, count(ip)
  as vw from log group by path order by vw desc;`
 
- `create view artcounter as select articles.title,
+ `replace view artcounter as select articles.title,
                  ipcounter.vw from
                  articles left join ipcounter on
                  ipcounter.path LIKE '%' ||
                  articles.slug || '%' order by vw desc;`
 
-  `create view artauth as select articles.title,
+  `replace view artauth as select articles.title,
               authors.name from articles join authors on
               authors.id = articles.author;`
 
-  `create view request as select time::timestamp::date,
+  `replace view request as select time::timestamp::date,
                count(*) as requests
               from log group by time::timestamp::date;`
 
-  `create view errorpd as select time::timestamp::date,
+  `replace view errorpd as select time::timestamp::date,
                count(time::timestamp::date) as errors
               from log where status='404 NOT FOUND'
               group by time::timestamp::date;`
 
-  `create view requesterror as select request.time,
+  `replace view requesterror as select request.time,
                     request.requests, errorpd.errors
                    from request left join errorpd on
                     request.time=errorpd.time;`
